3ffea9878e6e99b3c11cefe0481e0040d4420917
AnarkiXD <siso7667@gmail.com>
2022-09-13 18:27:50 +0800

    A

---
+++ src/settings.dee12.js
@@ -0,0 +1,1 @@
+window._CCSettings={platform:"web-mobile",groupList:["default"],collisionMatrix:[[true]],hasResourcesBundle:true,hasStartSceneBundle:false,remoteBundles:[],subpackages:[],launchScene:"db://assets/resources/Scenes/Loading.fire",orientation:"",jsList:["assets/Script/Libraries/CryptoJS/core-min.dda48.js"],bundleVers:{resources:"32186",internal:"d17e2",main:"93143"}};
